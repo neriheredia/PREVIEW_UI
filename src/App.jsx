d096a94c2d698152b52a560fa509c75515bd2066
neriheredia <developer.heredia@gmail.com>
2022-12-17 12:06:45 -0300

    feat: added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import Topbar from './components/topbar/Topbar';
 import Homepage from './pages/homepage/Homepage';
 import Login from './pages/login/Login';
@@ -9,31 +8,18 @@ import Write from './pages/write/Write';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-	const currentUser = false;
-
 	return (
 		<BrowserRouter>
-			<Fragment>
-				<Topbar />
-				<Routes>
-					<Route path='/' element={<Homepage />} />
-					<Route path='/posts' element={<Homepage />} />
-					<Route
-						path='/register'
-						element={currentUser ? <Homepage /> : <Register />}
-					/>
-					<Route
-						path='/login'
-						element={currentUser ? <Homepage /> : <Login />}
-					/>
-					<Route path='/post/:id' element={<Single />} />
-					<Route path='/write' element={currentUser ? <Write /> : <Login />} />
-					<Route
-						path='/settings'
-						element={currentUser ? <Settings /> : <Login />}
-					/>
-				</Routes>
-			</Fragment>
+			<Topbar />
+			<Routes>
+				<Route path='/' element={<Homepage />} />
+				<Route path='/posts' element={<Homepage />} />
+				<Route path='/register' element={<Register />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/post/:id' element={<Single />} />
+				<Route path='/write' element={<Write />} />
+				<Route path='/settings' element={<Settings />} />
+			</Routes>
 		</BrowserRouter>
 	);
 }
