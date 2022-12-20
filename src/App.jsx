6432767fc851698f1b7a0aff9fad084f2c0db045
neriheredia <developer.heredia@gmail.com>
2022-12-20 19:51:29 -0300

    feat: update preview

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,17 @@ import Write from './pages/write/Write';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-	const currentUser = false;
-
 	return (
 		<BrowserRouter>
 			<Fragment>
 				<Topbar />
 				<Routes>
 					<Route path='/' element={<Homepage />} />
-					<Route path='/posts' element={<Homepage />} />
-					<Route
-						path='/register'
-						element={currentUser ? <Homepage /> : <Register />}
-					/>
-					<Route
-						path='/login'
-						element={currentUser ? <Homepage /> : <Login />}
-					/>
+					<Route path='/register' element={<Register />} />
+					<Route path='/login' element={<Login />} />
 					<Route path='/post/:id' element={<Single />} />
-					<Route path='/write' element={currentUser ? <Write /> : <Login />} />
-					<Route
-						path='/settings'
-						element={currentUser ? <Settings /> : <Login />}
-					/>
+					<Route path='/write' element={<Write />} />
+					<Route path='/settings' element={<Settings />} />
 				</Routes>
 			</Fragment>
 		</BrowserRouter>
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,14 +1,19 @@
-import Post from "../post/Post";
-import "./posts.css";
+import Post from '../post/Post';
+import './posts.css';
+import foto1 from '../../assets/foto1.jpeg';
+import foto2 from '../../assets/foto2.webp';
+import foto3 from '../../assets/foto3.jpeg';
+import foto4 from '../../assets/foto4.webp';
+import foto5 from '../../assets/foto5.webp';
 
 export default function Posts() {
-  return (
-    <div className="posts">
-      <Post img="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-      <Post img="https://images.pexels.com/photos/6758029/pexels-photo-6758029.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-      <Post img="https://images.pexels.com/photos/6711867/pexels-photo-6711867.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
-      <Post img="https://images.pexels.com/photos/5490778/pexels-photo-5490778.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
-      <Post img="https://images.pexels.com/photos/4916559/pexels-photo-4916559.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
-    </div>
-  );
+	return (
+		<div className='posts'>
+			<Post img={foto1} />
+			<Post img={foto2} />
+			<Post img={foto3} />
+			<Post img={foto4} />
+			<Post img={foto5} />
+		</div>
+	);
 }
