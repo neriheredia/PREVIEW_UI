4974c5196962df4afc978133a8b7dfe3aabe8502
neriheredia <developer.heredia@gmail.com>
2022-12-21 17:50:02 -0300

    feat: test2

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,9 +7,9 @@ export default function Topbar() {
 		<div className='top'>
 			<div className='topLeft'>
 				<i className='topIcon fab fa-facebook-square'></i>
-				<i className='topIcon fab fa-instagram-square'></i>
+				{/* <i className='topIcon fab fa-instagram-square'></i>
 				<i className='topIcon fab fa-pinterest-square'></i>
-				<i className='topIcon fab fa-twitter-square'></i>
+				<i className='topIcon fab fa-twitter-square'></i> */}
 			</div>
 			<div className='topCenter'>
 				<ul className='topList'>
